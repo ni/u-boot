303c8e9ff7e23ab51c281b1bc3c962995bf57541
Joe Hershberger <joe.hershberger@ni.com>
2012-12-03 22:05:02 -0600

    nizynq: Switch over to the UBI environment
    Make u-boot absorb the old raw NAND environment partitions.
    Give u-boot a little breathing room in the writeboot command.
    Since writepartitions now clobbers the on-disk environment, make sure
    that we write the copy we have in RAM before resetting.
    
    Signed-off-by: Joe Hershberger <joe.hershberger@ni.com>

--- board/ni/criozynq/board.c
+++ board/ni/criozynq/board.c
@@ -166,7 +166,7 @@ int board_nand_init(struct nand_chip *nand_chip)
 	 * This is crappy (using 0 as the device), but this is what the Zynq
 	 * NAND driver does, so we do it too to make sure we get the same device
 	 */
-	off = CONFIG_ENV_OFFSET;
+	off = CONFIG_MTD_UBOOT_OFFSET + CONFIG_BOARD_SIZE_LIMIT;
 	size = nand_info[0].size - off;
 
 	nand_unlock(&nand_info[0], off, size, 0);
