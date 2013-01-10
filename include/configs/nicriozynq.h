cd0e6f1e49f758f557cfb23c70a459b1a811274a
Joe Hershberger <joe.hershberger@ni.com>
2013-01-10 14:07:14 -0600

    nizynq: Switch caching back to write-back
    Write-back caching is much faster, but some drivers can't handle it.
    
    Use write-back for the cache and disable the cache entirely when a
    driver can't handle it (such as Ethernet, for example).
    
    Signed-off-by: Joe Hershberger <joe.hershberger@ni.com>

--- include/configs/nicriozynq.h
+++ include/configs/nicriozynq.h
@@ -124,7 +124,6 @@
 #define CONFIG_SYS_ALT_MEMTEST
 #define CONFIG_SYS_MEMTEST_SCRATCH 0xFFFFF000
 #define CONFIG_CMD_CACHE
-#define CONFIG_SYS_ARM_CACHE_WRITETHROUGH
 
 /*
  * NAND Flash settings
@@ -252,6 +251,7 @@
 		"fi;\0" \
 	"recoverycmd=echo Entering recovery mode!; " \
 		"run markhardbootcomplete; " \
+		"dcache off; " \
 		"if test -n \\\\\"$forcedrecovery\\\\\"; " \
 		"then " \
 			"run ipresetcmd; " \
--- include/configs/nimyrio.h
+++ include/configs/nimyrio.h
@@ -116,7 +116,6 @@
 #define CONFIG_SYS_ALT_MEMTEST
 #define CONFIG_SYS_MEMTEST_SCRATCH 0xFFFFF000
 #define CONFIG_CMD_CACHE
-#define CONFIG_SYS_ARM_CACHE_WRITETHROUGH
 
 /*
  * NAND Flash settings
@@ -249,6 +248,7 @@
 		"fi;\0" \
 	"recoverycmd=echo Entering recovery mode!; " \
 		"run markhardbootcomplete; " \
+		"dcache off; " \
 		"if test -n \\\\\"$forcedrecovery\\\\\"; " \
 		"then " \
 			"run ipresetcmd; " \
