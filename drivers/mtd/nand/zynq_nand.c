1b58f909525762499e6032b196b4bfc7ca6891a6
Joe Hershberger <joe.hershberger@ni.com>
2013-03-20 04:44:55 -0500

    Xilinx: ARM: nand: Add support for accessing params
    It is helpful to ask the NAND details about its behavior.  This command
    support along with CONFIG_SYS_NAND_ONFI_DETECTION allows for better
    support of each NAND part.
    
    Signed-off-by: Joe Hershberger <joe.hershberger@ni.com>
    Signed-off-by: Jeff Westfahl <jeff.westfahl@ni.com>

--- drivers/mtd/nand/zynq_nand.c
+++ drivers/mtd/nand/zynq_nand.c
@@ -215,6 +215,7 @@ static struct xnandps_command_format xnandps_commands[] __devinitdata = {
 	{NAND_CMD_UNLOCK1, NAND_CMD_NONE, 3, NAND_CMD_NONE},
 	{NAND_CMD_UNLOCK2, NAND_CMD_NONE, 3, NAND_CMD_NONE},
 	{NAND_CMD_LOCK_STATUS, NAND_CMD_NONE, 3, NAND_CMD_NONE},
+	{NAND_CMD_PARAM, NAND_CMD_NONE, 1, NAND_CMD_NONE},
 	{NAND_CMD_NONE, NAND_CMD_NONE, 0, 0},
 	/* Add all the flash commands supported by the flash device and Linux */
 	/* The cache program command is not supported by driver because driver
