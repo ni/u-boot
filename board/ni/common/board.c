b20c5c268973ac23c2e0c57a494b1f6c2cc7313c
Jeff Westfahl <jeff.westfahl@ni.com>
2013-04-16 12:24:30 -0500

    nizynq: Implement enable_caches architecture hook
    Added an implementation of enable_caches that turns on the data cache.
    
    Signed-off-by: Jeff Westfahl <jeff.westfahl@ni.com>

---
+++ board/ni/common/board.c
@@ -0,0 +1,13 @@
+/*
+ * Just to satisfy init routines..
+ */
+
+#include <common.h>
+
+#ifndef CONFIG_SYS_DCACHE_OFF
+void enable_caches(void)
+{
+	/* Enable D-cache. I-cache is already enabled in start.S */
+	dcache_enable();
+}
+#endif
