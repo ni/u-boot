e24bb2103a5586a55c5dfdba9ca3d3bbef1a89fd
Joe Hershberger <joe.hershberger@ni.com>
2014-04-14 15:46:07 -0500

    nizynq: Split environment into common header
    Very much of the environment is common among all NI devices.  These
    should not all be redefined for each family.
    
    A few calls to USB device functions should silently fail on other
    targets but will find those variables on a sbRIO-9651.
    
    Signed-off-by: Joe Hershberger <joe.hershberger@ni.com>
    Signed-off-by: Keng Soon Cheah <keng.soon.cheah@ni.com>

--- include/configs/nisomzynq.h
+++ include/configs/nisomzynq.h
@@ -11,6 +11,7 @@
 #define CONFIG_DEVICE_CODE "775E"
 #define CONFIG_FPGA_DEVICE_CODE "775E"
 #define CONFIG_DEVICE_DESC "sbRIO-9651"
+#define CONFIG_PREFIXED_DEVICE_DESC "NI " CONFIG_DEVICE_DESC
 #define CONFIG_TARGET_CLASS "cRIO"
 
 #define CONFIG_NI_USB_PID "0x770D"
@@ -228,77 +229,31 @@
 #define CONFIG_SYS_LOAD_ADDR 0x8000000
 #define CONFIG_LOADADDR CONFIG_SYS_LOAD_ADDR
 
-#define READONLY_DEFAULT_ENV_FLAGS \
-	"consolecmd:so,ncoutport:do,ncinport:do,nc:so,sc:so," \
-	"fdt_high:xo,initrd_high:xo,TargetClass:so,DeviceDesc:so," \
-	"DeviceCode:xo,FPGADeviceCode:xo," \
-	"loadaddr:xo,verifyaddr:xo," \
-	"USBVendorID:xo,USBProductID:xo,USBProduct:so,USBDevice:xo," \
-	"backuppage:xo,backupserialoffset:xo,backupethaddroffset:xo," \
-	"backupeth1addroffset:xo,backupusbgadgetethaddroffset:xo," \
-	"backupeth3addroffset:xo," \
-	"boot_safemode:so,boot_runmode:so,consoleoutcmd:so," \
-	"recoverybootcmd:so,recoverycmd:so,fpgaloadcmd:so,ipresetcmd:so," \
-	"ipconfigcmd:so,markhardbootcomplete:so,stopwatchdog:so," \
-	"setlederrorstatus:so,readbootmode:so," \
-	"readsoftdip:so,readcplddip:so,evaldip:so,safemode_err:so," \
-	"fpga_err:so,recovery_err:so,updateenv:so,resetenv:so," \
-	"writepartitions:so,writeboot:so,writefsbl:so,writeuboot:so," \
-	"bootcmd:so,preboot:so,mtdids:so,mtdparts:so,"
+#define FDT_HIGH "0x17FFFFFF"
+#define INITRD_HIGH "0x17FF7FFF"
+#define VERIFY_ADDR "0x10000000"
 
-#define READONLY_MFG_ENV_VARS \
-	"serial#:xo,ethaddr:mc,eth1addr:mc,usbgadgetethaddr:mc,eth3addr:mc," \
-	"wirelessRegionFactory:so,wl12xxnvs:so,"
+#include "niresetenv.h"
 
-#define NET_TYPE_ENV_VARS \
-	"ipaddr:i,sipaddr:i,netmask:i,snetmask:i,gatewayip:i,sgatewayip:i," \
-	"ncip:i,mtu:d,"
+/* sbRIO-9651 uses usbgadgetethaddr for the USB Gadget Ethernet MAC. */
+#define USBGADGETETHADDR_SAVE ENV_SAVE(usbgadgetethaddr)
+#define USBGADGETETHADDR_RESTORE ENV_RESTORE(usbgadgetethaddr)
 
-#define CONFIG_ENV_FLAGS_LIST_STATIC \
-	READONLY_DEFAULT_ENV_FLAGS \
-	READONLY_MFG_ENV_VARS \
-	NET_TYPE_ENV_VARS
+/* sbRIO-9651 uses eth3addr for WiFi. */
+#define WIFIETHADDR_SAVE ENV_SAVE(eth3addr)
+#define WIFIETHADDR_RESTORE ENV_RESTORE(eth3addr)
 
-#define REAL_EXTRA_ENV_SETTINGS \
-	"autoload=n\0" \
-	"silent=1\0" \
-	"consolecmd=setenv console " CONFIG_CONSOLE_LINUX_DEV ",$baudrate\0" \
-	"ncoutport=7865\0" \
-	"ncinport=8473\0" \
-	"ncip=255.255.255.255\0" \
-	"nc=setenv stdout nc;setenv stdin nc\0" \
-	"sc=setenv stdout " CONFIG_CONSOLE_UBOOT_DEV ";" \
-		"setenv stderr " CONFIG_CONSOLE_UBOOT_DEV ";" \
-		"setenv stdin " CONFIG_CONSOLE_UBOOT_DEV ";\0" \
-	"fdt_high=0x17ffffff\0" \
-	"initrd_high=0x17ff7fff\0" \
-	"TargetClass=" CONFIG_TARGET_CLASS "\0" \
-	"DeviceDesc=" CONFIG_DEVICE_DESC "\0" \
-	"DeviceCode=0x" CONFIG_DEVICE_CODE "\0" \
-	"FPGADeviceCode=0x" CONFIG_FPGA_DEVICE_CODE "\0" \
-	"USBVendorID=" CONFIG_NI_USB_VID "\0" \
-	"USBProductID=" CONFIG_NI_USB_PID "\0" \
-	"USBProduct=sbRIO-9651\0" \
-	"USBDevice=0x" CONFIG_DEVICE_CODE "\0" \
-	"mtdids=" MTDIDS_DEFAULT "\0" \
-	"mtdparts=" MTDPARTS_DEFAULT "\0" \
-	"verifyaddr=0x10000000\0" \
-	"backuppage=" __stringify(CONFIG_BACKUP_PAGE) "\0" \
-	"backupserialoffset=" __stringify(CONFIG_BACKUP_SERIAL_OFFSET) "\0" \
-	"backupethaddroffset=" __stringify(CONFIG_BACKUP_ETHADDR_OFFSET) "\0" \
-	"backupeth1addroffset=" __stringify(CONFIG_BACKUP_ETH1ADDR_OFFSET) \
-				"\0" \
-	"backupusbgadgetethaddroffset=" \
-		__stringify(CONFIG_BACKUP_USBGADGETETHADDR_OFFSET) \
-		"\0" \
-	"backupeth3addroffset=" __stringify(CONFIG_BACKUP_ETH3ADDR_OFFSET) \
-				"\0" \
-	"wirelessRegionFactory=840\0" \
-	"wl12xxnvs=" CONFIG_DEFAULT_NVS "\0" \
-	"sdboot=echo Copying Safemode from SD to RAM...; " \
-		"mmcinfo; " \
-		"fatload mmc 0 $loadaddr linux_safemode.itb; " \
-		"source $loadaddr:bootscript;\0" \
+/* sbRIO-9651 uses ethaddr and eth1addr for wired Ethernet. */
+#define ETHADDR_SAVE ENV_SAVE(ethaddr) ENV_SAVE(eth1addr)
+#define ETHADDR_RESTORE ENV_RESTORE(ethaddr) ENV_RESTORE(eth1addr)
+
+/* sbRIO-9651 might have a wl12xx */
+#define WL12XXNVS_SAVE ENV_SAVE(wl12xxnvs)
+#define WL12XXNVS_RESTORE ENV_RESTORE(wl12xxnvs)
+
+#include "nicommonenv.h"
+
+#define USB_HOST_DEVICE_COMMANDS \
 	"read_usb0_otgsc_id=setexpr.l usb0_otgsc_id *" \
 		__stringify(XPSS_USB0_OTGCSR) \
 		" \\\\& 0x100\0" \
@@ -310,349 +265,12 @@
 			"fdt addr $fdtaddr && " \
 			"fdt set /amba@0/usb@e0002000 dr_mode host; " \
 		"fi;\0" \
-	"boot_safemode=" \
-		"if ubifsmount ubi:bootfs && " \
-		"ubifsload $loadaddr .safe/linux_safemode.itb && " \
-		"imi $loadaddr; then " \
-			"setenv verify n; " \
-			"run set_usb0_host_device_mode; " \
-			"source $loadaddr:bootscript; " \
-		"else " \
-			"echo $safemode_err; " \
-			"run recoverycmd; " \
-			"run recoverybootcmd;" \
-		"fi;\0" \
-	"boot_runmode=" \
-		"if ubifsmount ubi:bootfs && " \
-		"ubifsload $loadaddr linux_runmode.itb && " \
-		"imi $loadaddr; then " \
-			"setenv verify n; " \
-			"run set_usb0_host_device_mode; " \
-			"source $loadaddr:bootscript; " \
-		"else " \
-			"run boot_safemode; " \
-		"fi;\0" \
-	"consoleoutcmd=" \
-		"if test -n \\\\\"$isconsoleout\\\\\"; then " \
-			"run consolecmd; " \
-			"if test \\\\\"$bootdelay\\\\\" -gt 0; then " \
-				"setenv silent; " \
-				"setenv consoleparam console=$console; " \
-			"else " \
-				"setenv consoleparam console=$console quiet; " \
-			"fi; " \
-			"run sc; " \
-		"else " \
-			"setenv consoleparam console= quiet; " \
-			"setenv stderr nulldev; " \
-			"setenv stdin nulldev; " \
-			"setenv stdout nulldev; " \
-			"setenv bootdelay -2; " \
-		"fi;\0" \
-	"recoverybootcmd=" \
-		"if test -n \\\\\"$savebootcmd\\\\\"; " \
-		"then " \
-			"flags=${.flags}; " \
-			"setenv .flags bootcmd; " \
-			"setenv bootcmd \\\\\"$savebootcmd\\\\\"; " \
-			"setenv .flags $flags; " \
-		"fi; " \
-		"if test -n \\\\\"$savebootdelay\\\\\"; " \
-		"then " \
-			"setenv bootdelay $savebootdelay; " \
-		"fi;\0" \
-	"recoverycmd=echo Entering recovery mode!; " \
-		"run markhardbootcomplete; " \
-		"run stopwatchdog; " \
-		"dcache off; " \
-		"if test -n \\\\\"$forcedrecovery\\\\\"; " \
-		"then " \
-			"run resetenv && " \
-			"run ipresetcmd; " \
-		"fi;" \
-		"if usb start && " \
-			"fatload usb 0:auto $loadaddr recovery.cfg && " \
-			"iminfo && " \
-			"fdt addr $loadaddr && " \
-			"fdt get value recoverType /images/recover type && " \
-			"test $recoverType = script && " \
-			"fdt get value recoveryDeviceCode / DeviceCode && " \
-			"test $recoveryDeviceCode = $DeviceCode;" \
-		"then " \
-			"source $loadaddr:recover; " \
-		"elif test -n \\\\\"$forcedrecovery\\\\\"; " \
-		"then " \
-			"flags=${.flags} && " \
-			"setenv .flags bootcmd && " \
-			"savebootcmd=$bootcmd && " \
-			"setenv bootcmd \\\\\"$recoverybootcmd\\\\\" && " \
-			"setenv .flags $flags && " \
-			"savebootdelay=$bootdelay && " \
-			"setenv bootdelay -2 && " \
-			"run setlederrorstatus && " \
-			"usb reset && " \
-			"run ipconfigcmd; " \
-			"run nc; " \
-			"setenv silent; " \
-			"while sleep 1; do " \
-				"echo \\\"${DeviceCode:-<not-set>}, " \
-					"${serial#:-<not-set>}, " \
-					"${ethaddr:-<not-set>}, " \
-					"${ipaddr:-<not-set>}, " \
-					"${hostname:-<not-set>}, " \
-					"${comment:-<not-set>}\\\"; " \
-			"done; " \
-		"else " \
-			"run setlederrorstatus; " \
-			"echo $recovery_err; " \
-		"fi;\0" \
-	"fpgaloadcmd=ubifsmount ubi:bootfs; " \
-		"if test -n \\\\\"$isnofpgaapp\\\\\" -o $bootmode = safemode; then " \
-			"loaddefaultbit=1; " \
-		"else " \
-			"if ubifsload $verifyaddr user.bit.crc && " \
-				"ubifsload $loadaddr user.bit.bin && " \
-				"md5sum -v $loadaddr $filesize *$verifyaddr; " \
-			"then " \
-				"if fpga load 0 $loadaddr $filesize; then " \
-					"fpgasuccess=1; " \
-				"else " \
-					"echo $fpga_err..(user) ; " \
-					"loaddefaultbit=1; " \
-				"fi; " \
-			"else " \
-				"loaddefaultbit=1; " \
-			"fi; " \
-		"fi; " \
-		"if test -n \\\\\"$loaddefaultbit\\\\\"; then " \
-			"if ubifsload $verifyaddr .defbit/default.bit.crc && " \
-				"ubifsload $loadaddr .defbit/default.bit.bin && " \
-				"md5sum -v $loadaddr $filesize *$verifyaddr; " \
-			"then " \
-				"configfpga=1; " \
-			"fi; " \
-		"fi; " \
-		"if test -n \\\\\"$configfpga\\\\\"; then " \
-			"if fpga load 0 $loadaddr $filesize; then " \
-				"fpgasuccess=1; " \
-			"fi; " \
-		"fi; " \
-		"if test -z \\\\\"$fpgasuccess\\\\\"; then " \
-			"echo $fpga_err; " \
-			"run recoverycmd; " \
-			"run recoverybootcmd;" \
-		"fi; " \
-		"run markhardbootcomplete;\0" \
-	"ipresetcmd=echo Resetting primary Ethernet configuration; " \
-		"setenv dhcpenabled 1; " \
-		"setenv linklocalenabled 1; " \
-		"setenv sdnsip; " \
-		"setenv sgatewayip; " \
-		"setenv sipaddr; " \
-		"setenv snetmask; " \
-		"setenv mtu; " \
-		"setenv ncip 255.255.255.255; " \
-		"savesilent=$silent;setenv silent 1; " \
-		"saveenv; " \
-		"setenv silent $savesilent;\0" \
-	"ipconfigcmd=if test \\\\\"$dhcpenabled\\\\\" -ne 0 -a " \
-			"-z \\\\\"$forcedrecovery\\\\\";" \
-		"then " \
-			"dhcpfail=0; " \
-			"dhcp || dhcpfail=1;" \
-		"else " \
-			"dhcpfail=-1;" \
-		"fi;" \
-		"if test \\\\\"$linklocalenabled\\\\\" -ne 0 -a " \
-			"$dhcpfail -ne 0 -o " \
-			"-n \\\\\"$forcedrecovery\\\\\";" \
-		"then " \
-			"llfail=0;" \
-			"linklocal || llfail=1;" \
-		"else " \
-			"llfail=-1;" \
-		"fi;" \
-		"if test $llfail -ne 0 -a " \
-			"$dhcpfail -ne 0; " \
-		"then " \
-			"setenv ipaddr $sipaddr; " \
-			"setenv netmask $snetmask; " \
-			"setenv gatewayip $sgatewayip; " \
-		"fi;\0" \
-	"markhardbootcomplete=" \
-		"i2c mw 0x40 1 0x20;\0" \
-	"stopwatchdog=" \
-		"i2c mw 0x40 0x13 0x80;\0" \
-	"setlederrorstatus=" \
-		"i2c mw 0x40 0x5 0xAA; " \
-		"i2c mw 0x40 0x6 0xAA;\0" \
-	"readbootmode=i2c read 0x40 0x1F 1 $loadaddr; " \
-		"setexpr.b bootmodeval *$loadaddr \\\\& 0x3; " \
-		"if test $bootmodeval -eq 1; then " \
-			"bootmode=safemode; " \
-		"else " \
-			"bootmode=runmode; " \
-		"fi; " \
-		"setenv bootmodeval;\0" \
-	"readsoftdip=if ubifsmount ubi:config && " \
-		"ubifsload $loadaddr ni-rt.ini && " \
-		"ini systemsettings; then " \
-			"setenv safemode.enabled ${safemode.enabled:-false}; " \
-			"setenv ipreset.enabled ${ipreset.enabled:-false}; " \
-			"setenv consoleout.enabled ${consoleout.enabled:-false}; " \
-			"setenv nofpgaapp.enabled ${nofpgaapp.enabled:-false}; " \
-		"else " \
-			"setenv safemode.enabled false; " \
-			"setenv ipreset.enabled false; " \
-			"setenv consoleout.enabled false; " \
-			"setenv nofpgaapp.enabled false; " \
-		"fi;\0" \
-	"readcplddip=i2c read 0x40 1 1 $loadaddr; " \
-		"setexpr.b cpld.safemode *$loadaddr \\\\& 0x01; " \
-		"setexpr.b cpld.ipreset *$loadaddr \\\\& 0x02; " \
-		"setexpr.b cpld.consoleout *$loadaddr \\\\& 0x04; " \
-		"setexpr.b cpld.softboot *$loadaddr \\\\& 0x20; " \
-		"i2c read 0x40 4 1 $loadaddr; " \
-		"setexpr.b cpld.resetbybutton *$loadaddr \\\\& 0x01;\0" \
-	"evaldip=" \
-		"if test ${cpld.safemode} -ne 0 -o " \
-			"\\\\\"${safemode.enabled}\\\\\" != false -o " \
-			"\\\\\"${bootmode}\\\\\" = safemode; " \
-		"then " \
-			"issafemode=1; " \
-			"bootmode=safemode; " \
-		"fi; " \
-		"if test ${cpld.ipreset} -ne 0 -o " \
-			"\\\\\"${ipreset.enabled}\\\\\" != false; " \
-		"then " \
-			"isipreset=1; " \
-		"fi; " \
-		"if test ${cpld.consoleout} -ne 0 -o " \
-			"\\\\\"${consoleout.enabled}\\\\\" != false; " \
-		"then " \
-			"isconsoleout=1; " \
-		"fi; " \
-		"if test \\\\\"${nofpgaapp.enabled}\\\\\" != false; then " \
-			"isnofpgaapp=1; " \
-		"fi; " \
-		"if test ${cpld.softboot} -eq 0 -a " \
-			"${cpld.resetbybutton} -ne 0 -a " \
-			"${cpld.safemode} -ne 0; " \
-		"then " \
-			"isforcedrecoverymode=1; " \
-		"fi; " \
-		"setenv safemode.enabled; " \
-		"setenv ipreset.enabled; " \
-		"setenv consoleout.enabled; " \
-		"setenv nofpgaapp.enabled; " \
-		"setenv noapp.enabled; " \
-		"setenv host_name; " \
-		"setenv primarymac; " \
-		"setenv language; " \
-		"setenv sshd.enabled; " \
-		"setenv ubifs_writeback_centisecs; " \
-		"setenv ubifs_expire_centisecs; " \
-		"setenv cpld.safemode; " \
-		"setenv cpld.ipreset; " \
-		"setenv cpld.consoleout; " \
-		"setenv cpld.softboot; " \
-		"setenv cpld.resetbybutton;\0" \
-	"safemode_err=Failed to find a valid safemode image.\0" \
-	"fpga_err=Failed to load an FPGA image.\0" \
-	"recovery_err=Safemode or FPGA is corrupt. " \
-		"Insert recovery USB and reboot.\0" \
-	"updateenv=env export -b $loadaddr; " \
-		"env default -a; " \
-		"env import -b $loadaddr;\0" \
-	"resetenv=" \
-		"serial_save=${serial#} && " \
-		"ethaddr_save=$ethaddr && " \
-		"eth1addr_save=$eth1addr && " \
-		"usbgadgetethaddr_save=$usbgadgetethaddr && " \
-		"eth3addr_save=$eth3addr && " \
-		"wirelessRegionFactory_save=$wirelessRegionFactory && " \
-		"wl12xxnvs_save=$wl12xxnvs && " \
-		"env default -a && " \
-		"env set serial# $serial_save && " \
-		"env set ethaddr $ethaddr_save && " \
-		"env set eth1addr $eth1addr_save && " \
-		"env set usbgadgetethaddr $usbgadgetethaddr_save && " \
-		"env set eth3addr $eth3addr_save && " \
-		"env set wirelessRegionFactory $wirelessRegionFactory_save && " \
-		"env set wl12xxnvs $wl12xxnvs_save;\0" \
-	"writepartitions=" \
-		"if ubi part boot-config && " \
-			"ubi read $verifyaddr u-boot-env1 1 && " \
-			"ubi read $verifyaddr u-boot-env2 1; " \
-		"then " \
-			"ubi remove bootfs && " \
-			"ubi remove config; " \
-		"else " \
-			"nand erase.part boot-config && " \
-			"ubi part boot-config && " \
-			"ubi create u-boot-env1 " __stringify(CONFIG_ENV_SIZE) " dynamic && " \
-			"ubi create u-boot-env2 " __stringify(CONFIG_ENV_SIZE) " dynamic; " \
-		"fi && " \
-		"ubi create bootfs " __stringify(CONFIG_BOOTFS_VOLUME_SIZE) " dynamic && " \
-		"ubi create config - dynamic && " \
-		"if ubi part root && " \
-			"ubi read $verifyaddr rootfs 1; " \
-		"then " \
-			"ubi remove rootfs; " \
-		"else " \
-			"nand erase.part root && " \
-			"ubi part root; " \
-		"fi && " \
-		"ubi create rootfs - dynamic;\0" \
-	"writeboot=nand erase 0 " __stringify(CONFIG_BOOT_BIN_SIZE_LIMIT) "; " \
-		"nand write $loadaddr 0 " __stringify(CONFIG_BOOT_BIN_SIZE_LIMIT) "; " \
-		"nand read $verifyaddr 0 " __stringify(CONFIG_BOOT_BIN_SIZE_LIMIT) "; " \
-		"cmp.b $loadaddr $verifyaddr " __stringify(CONFIG_BOOT_BIN_SIZE_LIMIT) ";\0" \
-	"writefsbl=nand erase.part fsbl; " \
-		"nand write $loadaddr fsbl; " \
-		"nand read $verifyaddr fsbl; " \
-		"cmp.b $loadaddr $verifyaddr " __stringify(CONFIG_MTD_UBOOT_OFFSET) ";\0" \
-	"writeuboot=nand erase.part u-boot; " \
-		"nand write $loadaddr u-boot; " \
-		"nand read $verifyaddr u-boot; " \
-		"cmp.b $loadaddr $verifyaddr " \
-			__stringify(CONFIG_BOARD_SIZE_LIMIT) ";\0"
-
-#define REAL_BOOTCOMMAND \
-	"if test $bootmode = safemode -o $bootmode = install; then " \
-		"run boot_safemode; " \
-	"else " \
-		"run boot_runmode; "\
-	"fi"
-
-#define REAL_PREBOOT \
-	"setenv silent 1; " \
-	"nand lock tight; " \
-	"ubi part boot-config; " \
-	"run readsoftdip; " \
-	"run readcplddip; " \
-	"run readbootmode; " \
-	"run read_usb0_otgsc_id;" \
-	"run evaldip; " \
-	"if test -n \\\\\"$isforcedrecoverymode\\\\\"; then " \
-		"if test -n \\\\\"$isconsoleout\\\\\"; then " \
-			"setenv silent; " \
-		"fi; " \
-		"forcedrecovery=1; " \
-		"run recoverycmd; " \
-	"else " \
-		"run fpgaloadcmd; " \
-		"run consoleoutcmd; " \
-		"if test -n \\\\\"$isipreset\\\\\"; then " \
-			"run ipresetcmd; " \
-		"fi; " \
-	"fi;"
-
 
 #if defined(CONFIG_MFG)
 
 #undef CONFIG_EXTRA_ENV_SETTINGS
 #define CONFIG_EXTRA_ENV_SETTINGS \
+	USB_HOST_DEVICE_COMMANDS \
 	"ipaddr=192.168.1.180\0" \
 	"netmask=255.255.255.0\0" \
 	"gatewayip=192.168.1.185\0" \
@@ -669,6 +287,7 @@
 
 #undef CONFIG_EXTRA_ENV_SETTINGS
 #define CONFIG_EXTRA_ENV_SETTINGS \
+	USB_HOST_DEVICE_COMMANDS \
 	REAL_EXTRA_ENV_SETTINGS
 
 #undef CONFIG_BOOTCOMMAND
