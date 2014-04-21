61bcbe8e3cdf63828b45ce4d2b544d4025d9a6e6
Joe Hershberger <joe.hershberger@ni.com>
2014-04-21 14:21:31 -0500

    tools: fw_setenv: Increase script buffer size
    NI's scripts can get out of control... recoverycmd is 1.5k
    
    Signed-off-by: Joe Hershberger <joe.hershberger@ni.com>

--- tools/env/fw_env.c
+++ tools/env/fw_env.c
@@ -495,7 +495,7 @@ int fw_setenv(int argc, char *argv[])
 int fw_parse_script(char *fname)
 {
 	FILE *fp;
-	char dump[1024];	/* Maximum line length in the file */
+	char dump[4096];	/* Maximum line length in the file */
 	char *name;
 	char *val;
 	int lineno = 0;
