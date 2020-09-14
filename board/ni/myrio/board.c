c3478c5e5b9758746be8e9742f6f293901bbdd07
Chen Yee Chew <chen.yee.chew@ni.com>
2020-09-14 16:58:44 +0800

    nimyrio: Enable DM I2C on NI cRIO targets
    Integrate from commit: f835bbf74f3e50b329a5f3245c9f0b02e11c25c5
    
    Signed-off-by: Kim Khiam Lim <kim.khiam.lim@ni.com>
    Reviewed-by: Keng Soon Cheah <keng.soon.cheah@ni.com>
    ---
    Natinst-Reviewboard-ID: 353896

--- board/ni/myrio/board.c
+++ board/ni/myrio/board.c
@@ -46,6 +46,14 @@ int board_init(void)
 int board_late_init (void)
 {
 	u8 tmp;
+	int err;
+	struct udevice *cpld;
+	err = i2c_get_chip_for_busnum(0, 0x40, 1, &cpld);
+	if (err) {
+		debug("%s: Cannot find CPLD\n", __func__);
+		cpld = NULL;
+	}
+
 #ifndef CONFIG_MFG
 	int serial_missing;
 	int ethaddr_missing;
@@ -60,8 +68,10 @@ int board_late_init (void)
 	/*
 	 * Take usb phy out of reset
 	 */
-	tmp = 0x00;
-	i2c_write(0x40, 0x03, 1, &tmp, 1);
+	if (NULL != cpld) {
+		tmp = 0x00;
+		dm_i2c_write(cpld, 0x03, &tmp, 1);
+	}
 
 #if defined(CONFIG_MFG)
 	set_default_env("Default env required for manufacturing.\n");
@@ -188,10 +198,19 @@ int dram_init(void)
 void reset_misc(void)
 {
 	u8 tmp;
+	int err;
+	struct udevice *dev;
 
+	err = i2c_get_chip_for_busnum(0, 0x40, 1, &dev);
+	if (err) {
+		debug("%s: Cannot find CPLD\n", __func__);
+		return;
+	}
 	/* Reset the target using the CPLD */
-	tmp = 0x80;
-	i2c_write(0x40, 0x02, 1, &tmp, 1);
+	if (NULL != dev){
+		tmp = 0x80;
+		dm_i2c_write(dev, 0x02, &tmp, 1);
+	}
 
 	while(1) {;}
 }
